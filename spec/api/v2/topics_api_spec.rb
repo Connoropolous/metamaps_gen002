d4d1a15f5024dcd4e5bb0dee519177f6152203bd
Devin Howard <devin@callysto.com>
2016-09-03 16:24:50 +0800

    Update topic spec for api v2

--- spec/api/v2/topics_api_spec.rb
+++ spec/api/v2/topics_api_spec.rb
@@ -5,37 +5,37 @@ RSpec.describe 'topics API', type: :request do
   let(:token) { create(:token, user: user).token }
   let(:topic) { create(:topic, user: user) }
 
-  it 'GET /api/v1/topics' do
+  it 'GET /api/v2/topics' do
     create_list(:topic, 5)
-    get '/api/v1/topics', params: { access_token: token }
+    get '/api/v2/topics', params: { access_token: token }
 
     expect(response).to have_http_status(:success)
     expect(response).to match_json_schema(:topics)
   end
 
-  it 'GET /api/v1/topics/:id' do
-    get "/api/v1/topics/#{topic.id}"
+  it 'GET /api/v2/topics/:id' do
+    get "/api/v2/topics/#{topic.id}"
 
     expect(response).to have_http_status(:success)
     expect(response).to match_json_schema(:topic)
   end
 
-  it 'POST /api/v1/topics' do
-    post '/api/v1/topics', params: { topic: topic.attributes, access_token: token }
+  it 'POST /api/v2/topics' do
+    post '/api/v2/topics', params: { topic: topic.attributes, access_token: token }
 
     expect(response).to have_http_status(:success)
     expect(response).to match_json_schema(:topic)
   end
 
-  it 'PATCH /api/v1/topics/:id' do
-    patch "/api/v1/topics/#{topic.id}", params: { topic: topic.attributes, access_token: token }
+  it 'PATCH /api/v2/topics/:id' do
+    patch "/api/v2/topics/#{topic.id}", params: { topic: topic.attributes, access_token: token }
 
     expect(response).to have_http_status(:success)
     expect(response).to match_json_schema(:topic)
   end
 
-  it 'DELETE /api/v1/topics/:id' do
-    delete "/api/v1/topics/#{topic.id}", params: { access_token: token }
+  it 'DELETE /api/v2/topics/:id' do
+    delete "/api/v2/topics/#{topic.id}", params: { access_token: token }
 
     expect(response).to have_http_status(:no_content)
   end
