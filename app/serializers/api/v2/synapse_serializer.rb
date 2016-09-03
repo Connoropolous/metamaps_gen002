f67aa898cf4f4c9a4b5935a5bbd6875bd76c7a3b
Devin Howard <devin@callysto.com>
2016-09-03 15:10:56 +0800

    fill out synapse api

--- app/serializers/api/v2/synapse_serializer.rb
+++ app/serializers/api/v2/synapse_serializer.rb
@@ -4,14 +4,9 @@ module Api
       attributes :id,
         :desc,
         :category,
-        :weight,
         :permission,
         :created_at,
         :updated_at
-
-      #has_one :topic1, root: :topics, serializer: TopicSerializer
-      #has_one :topic2, root: :topics, serializer: TopicSerializer
-      #has_one :user, serializer: UserSerializer
 
       def self.embeddable
         {
