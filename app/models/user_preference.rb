fb88c31ca9586c4bb4b741bd37cb149f51a4ae1c
Connor Turland <connorturland@gmail.com>
2016-09-09 20:35:04 -0400

    Update user_preference.rb

--- app/models/user_preference.rb
+++ app/models/user_preference.rb
@@ -3,7 +3,7 @@ class UserPreference
 
   def initialize
     array = []
-    %w(Action, Aim, Idea, Question, Note, Wildcard, Subject).each do |m|
+    %w(Action Aim Idea Question Note Wildcard Subject).each do |m|
       metacode = Metacode.find_by_name(m)
       array.push(metacode.id.to_s) if metacode
     end
