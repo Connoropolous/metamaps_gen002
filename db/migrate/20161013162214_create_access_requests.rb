5e97898a4b261077bdad87a6d183bf4f14b7c359
Connor Turland <connorturland@gmail.com>
2016-10-16 20:09:45 -0400

    set default values for approved and answered

--- db/migrate/20161013162214_create_access_requests.rb
+++ db/migrate/20161013162214_create_access_requests.rb
@@ -2,8 +2,8 @@ class CreateAccessRequests < ActiveRecord::Migration[5.0]
   def change
     create_table :access_requests do |t|
       t.references :user, foreign_key: true
-      t.boolean :approved
-      t.boolean :answered
+      t.boolean :approved, default: false
+      t.boolean :answered, default: false
       t.references :map, foreign_key: true
 
       t.timestamps
