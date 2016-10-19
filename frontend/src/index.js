e545c3e69e07abac15714178f780f0ebb40f7540
Connor Turland <connorturland@gmail.com>
2016-10-18 20:09:32 -0400

    don't need it because socketioconnection is being imported now

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,6 @@
 // create global references
 import _ from 'underscore'
-import io from 'socket.io-client'
 import Metamaps from './Metamaps'
 
 window._ = _
-window.io = io
 window.Metamaps = Metamaps
