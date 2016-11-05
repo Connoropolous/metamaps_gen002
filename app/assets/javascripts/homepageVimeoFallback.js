4c6f2cfe31b72c9642a5987e8376486a2e693b90
Devin Howard <devin@callysto.com>
2016-11-05 11:10:42 -0400

    eslint

--- app/assets/javascripts/homepageVimeoFallback.js
+++ app/assets/javascripts/homepageVimeoFallback.js
@@ -1,27 +1,29 @@
+/* global $ */
+
 $(document).ready(function () {
-  if (window.location.pathname == "/") {
+  if (window.location.pathname === '/') {
     $.ajax({
-      type: "GET",
-      url: '//player.vimeo.com',
-      error: function(e) {
-        $('.homeVideo').hide();
+      type: 'GET',
+      url: 'https://player.vimeo.com',
+      error: function (e) {
+        $('.homeVideo').hide()
         $('.homeVideo').replaceWith($('<video/>', {
           poster: '/assets/metamaps-intro-poster.webp',
           width: '560',
           height: '315',
           class: 'homeVideo',
           controls: ''
-        }));
+        }))
         $('.homeVideo').append($('<source/>', {
-          src: '//metamaps.cc/videos/metamaps-intro.mp4',
+          src: 'https://metamaps.cc/videos/metamaps-intro.mp4',
           type: 'video/mp4'
-        }));
+        }))
         $('.homeVideo').append(
           '<p>You can watch our instruction video at ' +
           '<a href="https://metamaps.cc/videos/metamaps-intro.mp4">' +
           'https://metamaps.cc/videos/metamaps-intro.mp4</a>.'
-        );
+        )
       }
-    });
-  }//if
-});
+    })
+  }// if
+})
