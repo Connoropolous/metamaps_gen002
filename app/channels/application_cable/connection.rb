50e7e400e13689b43a0548501265be2e44eee96b
Connor Turland <connorturland@gmail.com>
2017-01-03 15:14:40 -0500

    rubocop fixes

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :current_user
@@ -8,13 +9,14 @@ module ApplicationCable
     end
 
     protected
-      def find_verified_user
-        verified_user = User.find_by(id: cookies.signed['user.id'])
-        if verified_user && cookies.signed['user.expires_at'] > Time.now
-          verified_user
-        else
-          reject_unauthorized_connection
-        end
+
+    def find_verified_user
+      verified_user = User.find_by(id: cookies.signed['user.id'])
+      if verified_user && cookies.signed['user.expires_at'] > Time.now.getlocal
+        verified_user
+      else
+        reject_unauthorized_connection
       end
+    end
   end
 end
