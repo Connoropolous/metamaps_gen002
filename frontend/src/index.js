3662f4c6fc76eeed159cbaaebe55db2023e9e465
Devin Howard <devin@callysto.com>
2017-01-19 15:26:52 -0500

    install jquery.ajaxq from npm

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 // create global references
 import _ from 'lodash'
 import Metamaps from './Metamaps'
-import 'jquery.ajaxq' // allow queueing ajax requests so it doesn't overload
 
 window._ = _
 window.Metamaps = Metamaps
