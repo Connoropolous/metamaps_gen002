6edcbc58abbe54a72f5cb18bdcb4552560657186
Devin Howard <devin@callysto.com>
2017-01-21 13:42:34 -0500

    patch ajaxq into Backbone code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,13 @@
-// create global references
 import _ from 'lodash'
+import Backbone from 'backbone'
+
+// make changes to Backbone before loading Metamaps code
+try { Backbone.$ = window.$ } catch(err) {}
+Backbone.ajax = window.$.ajaxq
+
 import Metamaps from './Metamaps'
 
+// create global references
 window._ = _
 window.Metamaps = Metamaps
+window.Backbone = Backbone // TODO remove
