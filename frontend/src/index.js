4d20723be162947768ad2abe19b064f4dab0b1f4
Devin Howard <devin@callysto.com>
2017-01-21 15:27:56 -0500

    eslint

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,11 @@
-import _ from 'lodash'
-import Backbone from 'backbone'
-
 // make changes to Backbone before loading Metamaps code
-try { Backbone.$ = window.$ } catch(err) {}
+import Backbone from 'backbone'
+try { Backbone.$ = window.$ } catch (err) {}
 Backbone.ajax = (opts) => window.$.ajaxq('backbone-ajaxq', opts)
 
+import _ from 'lodash'
 import Metamaps from './Metamaps'
 
 // create global references
 window._ = _
 window.Metamaps = Metamaps
-window.Backbone = Backbone // TODO remove
