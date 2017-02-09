53a5ea76c18b9d953e72d736674bd9ec06f73412
Devin Howard <devin@callysto.com>
2017-02-08 20:16:43 -0800

    helper function for messages from devs

--- app/services/notification_service.rb
+++ app/services/notification_service.rb
@@ -17,13 +17,24 @@ class NotificationService
 
   def self.access_approved(request)
     body = renderer.render(template: 'map_mailer/access_approved_email', locals: { map: request.map }, layout: false)
-    receipt = request.user.notify(request.approved_text, body, request, false, MAILBOXER_CODE_ACCESS_APPROVED, true, request.map.user)
+    request.user.notify(request.approved_text, body, request, false, MAILBOXER_CODE_ACCESS_APPROVED, true, request.map.user)
   end
 
   def self.invite_to_edit(map, inviter, invited)
     user_map = UserMap.find_by(user: invited, map: map)
     body = renderer.render(template: 'map_mailer/invite_to_edit_email', locals: { map: map, inviter: inviter }, layout: false)
     invited.notify(map.invited_text, body, user_map, false, MAILBOXER_CODE_INVITE_TO_EDIT, true, inviter)
+  end
+
+  # note: this is a global function, probaobly called from the rails console with some html body
+  def self.message_from_devs(subject, body, opts = {})
+    users = opts[:users] || User.all
+    obj = opts[:obj] || nil
+    sanitize_text = opts[:sanitize_text] || false
+    notification_code = opts[:notification_code] || MAILBOXER_CODE_MESSAGE_FROM_DEVS
+    send_mail = opts[:send_mail] || true
+    sender = opts[:sender] || User.find_by_email('ishanshapiro@gmail.com')
+    Mailboxer::Notification.notify_all(users, subject, body, obj, sanitize_text, notification_code, send_mail, sender)
   end
 
   def self.text_for_notification(notification)
@@ -37,7 +48,7 @@ class NotificationService
       map = notification.notified_object&.map
       'gave you edit access to map  <span class="in-bold">' + map.name + '</span>'
     elsif notification.notification_code == MAILBOXER_CODE_MESSAGE_FROM_DEVS
-      strip_tags(notification.body).truncate(70)
+      notification.subject
     end
   end
 end
