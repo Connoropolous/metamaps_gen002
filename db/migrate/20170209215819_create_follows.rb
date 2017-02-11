de53e19d089dbcec03fea0faa12060664a029019
Connor Turland <connorturland@gmail.com>
2017-02-11 00:01:20 -0500

    Update 20170209215819_create_follows.rb

--- db/migrate/20170209215819_create_follows.rb
+++ db/migrate/20170209215819_create_follows.rb
@@ -2,9 +2,6 @@ class CreateFollows < ActiveRecord::Migration[5.0]
   def change
     create_table :follows do |t|
       t.references  :user, index: true
-      t.boolean     :email, default: true
-      t.boolean     :app, default: true
-      t.boolean     :push, default: true
       t.references  :followed, polymorphic: true, index: true
       t.timestamps
     end
