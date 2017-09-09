c0021c7d40b18c73f500d346e566cc58a22ead82
Devin Howard <devin@callysto.com>
2017-09-08 23:39:13 -0700

    fix sinon

--- frontend/test_support/dom.js
+++ frontend/test_support/dom.js
@@ -1,14 +1,13 @@
 const jsdom = require('jsdom')
+const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
+const win = doc.defaultView
 
-const { JSDOM } = jsdom
-const dom = new JSDOM('<!doctype html><html><body></body></html>')
-
-global.document = dom.window.document
-global.window = dom.window
+global.document = doc
+global.window = win
 
 // take all properties of the window object and also attach it to the
 // mocha global object
-propagateToGlobal(dom.window)
+propagateToGlobal(win)
 
 // from mocha-jsdom https://github.com/rstacruz/mocha-jsdom/blob/master/index.js#L80
 function propagateToGlobal(window) {
